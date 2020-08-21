b96a82f31691179c778c75d45eb8dfeab6147bf1
Pablo Muralles <pablomuralles28@gmail.com>
2020-08-21 12:17:26 -0600

    se cambio el metodo analizador por identificador de lexemas, se quito el .trim del metodo lectura archivo

--- MiniCompilador/Análisis Léxico/Analisis.cs
+++ MiniCompilador/Análisis Léxico/Analisis.cs
@@ -12,51 +12,65 @@ namespace MiniCompilador.Análisis_Léxico
 {
     class Analisis
     {
-
+        /// <summary>
+        /// Metodo para poder leer el archivo que ingresa el usuriario
+        /// </summary>
+        /// <param name="path"></param>
         public void LecturaArchivo(string path)
         {
-            int contadorLinea = 0;
+            var lexemas = new Dictionary<string, string>();
+            int contadorLinea = 1;
             var archivo = new StreamReader(path);
             var linea = archivo.ReadLine();
             while (linea != null)
             {
-                linea = linea.Trim();
-                Analizador(linea);
+                //linea = linea.Trim();
+                IdentificadorLexemas(linea,contadorLinea,lexemas);
                 contadorLinea++;
                 linea = archivo.ReadLine();
             }
         }
-
-        private void Analizador(string Cadena)
+        /// <summary>
+        /// Metodo que va identificar los lexemas del archivo de entrada
+        /// </summary>
+        /// <param name="Cadena"></param>
+        /// <param name="linea_"></param>
+        /// <param name="lexemas_"></param>
+        private void IdentificadorLexemas(string Cadena,int linea_, Dictionary<string, string> lexemas_)
         {
-           var listaMatch = new List<string>();
-            string dato = string.Empty;
-            var objExpreciones = new Expreciones();
             
+            string dato = string.Empty;
+            var listaCaracteres = Cadena.ToList();
+            var objExpreciones = new Expreciones();
 
-            foreach (var item in Cadena)
+            for (int i = 0; i < listaCaracteres.Count(); i++)
             {
-                if (!string.IsNullOrWhiteSpace(item.ToString()))
+                if (listaCaracteres[i].ToString() != " ")
                 {
-                    dato += item.ToString();
-                    (bool validacion, int id) = Validar(dato, objExpreciones);
-
-                    if (validacion == true && id != 2 )
+                    dato += listaCaracteres[i].ToString();
+                    if (i+1 < listaCaracteres.Count())
                     {
-                        listaMatch.Add(dato);
+                        if (objExpreciones.caracteres_.IsMatch(listaCaracteres[i+1].ToString())|| listaCaracteres[i+1].ToString() == " ")
+                        {
+                            lexemas_.Add(dato, $"{linea_}");
+                            dato = string.Empty;
+                        }
+                    }
+                    else
+                    {
+                        lexemas_.Add(dato, $"{linea_}");
                         dato = string.Empty;
                     }
+                    
 
-                }
-                else
-                {
-                    // validar si existe algo en dato si existe mostar error ya que no se reconocio nada
                 }
                 
-
-                 
             }
+            
+             
         }
+
+
         /// <summary>
         /// Metodo que valida con las expresiones regulares para ve en cual casa
         /// 1 = palabrasReservadas,2=identificador,3=booleana,4=entero,5=hexadecimal,6=double,7=cadena
@@ -64,7 +78,6 @@ namespace MiniCompilador.Análisis_Léxico
         /// <param name="cadena">caracter o cadena a evaluar</param>
         /// <param name="objExpreciones_">el objeto de la clase expresiones</param>
         /// <returns>verdadero si caso con alguna y un int que seria el identificador con cual caso</returns>
-
         private (bool,int) Validar(string cadena, Expreciones objExpreciones_)
         {
             
