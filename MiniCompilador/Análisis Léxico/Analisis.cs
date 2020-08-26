a1df231200fa323e1800a17075333338571af7c9
Pablo Muralles <pablomuralles28@gmail.com>
2020-08-26 12:38:03 -0600

    se quitaron librerias no necesarias y se juntos toda la fase 1

--- MiniCompilador/Análisis Léxico/Analisis.cs
+++ MiniCompilador/Análisis Léxico/Analisis.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Windows.Forms.VisualStyles;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows.Forms;
 
 namespace MiniCompilador.Análisis_Léxico
 {
