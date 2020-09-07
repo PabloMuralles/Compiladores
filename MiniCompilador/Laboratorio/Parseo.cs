be7fffe9a10de009b6edf2ffdcbe5fd879ad6069
Pablo Muralles <pablomuralles28@gmail.com>
2020-09-07 17:52:39 -0600

    se realizo el constructor de las clases y se hizo el metood match tokens en clase parseo

--- MiniCompilador/Laboratorio/Parseo.cs
+++ MiniCompilador/Laboratorio/Parseo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,48 @@ namespace MiniCompilador.Laboratorio
 {
     class Parseo
     {
-        public void Lectura_Token(List<Tuple<string,string>> Tockens) 
+
+
+        private List<Tuple<string, string>> tokens = new List<Tuple<string, string>>();
+
+        private string lookahead = string.Empty;
+
+        private int contador = 0;
+
+        private List<string> errores = new List<string>();
+
+        
+
+        private void MatchToken(string expectedToken)
         {
-         
+            try
+            {
+                if (lookahead == expectedToken)
+                {
+                    contador++;
+                    lookahead = tokens[contador].Item1;
+                }
+                else
+                {
+                    errores.Add($"Error sintactico: se esperaba {expectedToken} y se tenia {lookahead}. {tokens[contador == 0 ? contador : contador -1].Item2} ");
+                }
+            }
+            catch (Exception)
+            {
+                // se terminaron los tokens a leer 
+                throw;
+            }
         }
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="tokens_">Lista de tuplas generada en el analisis lexico</param>
+        public Parseo(List<Tuple<string, string>> tokens_)
+        {
+            tokens = tokens_;
+            lookahead = tokens[contador].Item1;
+        }
+
     }
 }
