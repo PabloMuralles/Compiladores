31c52a570dfe9a9dfe7a2875875afe754add70a5
Pablo Muralles <pablomuralles28@gmail.com>
2020-09-07 18:23:41 -0600

    se agrego metodo para poder obtener la lines y columnas de donde se encuentra el error esto en el clase de perseo

--- MiniCompilador/Laboratorio/Parseo.cs
+++ MiniCompilador/Laboratorio/Parseo.cs
@@ -32,7 +32,7 @@ namespace MiniCompilador.Laboratorio
                 }
                 else
                 {
-                    errores.Add($"Error sintactico: se esperaba {expectedToken} y se tenia {lookahead}. {tokens[contador == 0 ? contador : contador -1].Item2} ");
+                    errores.Add($"Error sintactico: se esperaba {expectedToken} y se tenia {lookahead}. {ObtenerUbicacion(tokens[contador == 0 ? contador : contador -1].Item2)}");
                 }
             }
             catch (Exception)
@@ -40,6 +40,25 @@ namespace MiniCompilador.Laboratorio
                 // se terminaron los tokens a leer 
                 throw;
             }
+        }
+
+        /// <summary>
+        /// Metodo para poder mostrar la linea y la columna donde se encuentra el error
+        /// </summary>
+        /// <param name="cadena">el item 2 de la lista de tuplas de tokens</param>
+        /// <returns>la linea y columnas donde se encuentra el error</returns>
+        private string ObtenerUbicacion(string cadena)
+        {
+            var split = cadena.Split(',');
+
+            var newCadena = string.Empty;
+
+            for (int i = 1; i < split.Length; i++)
+            {
+                newCadena += split[i] + " ";
+            }
+
+            return newCadena;
         }
 
         /// <summary>
