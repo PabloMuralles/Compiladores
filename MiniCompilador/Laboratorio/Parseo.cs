14027d30386498420b1730a45f7f9cfb556a6829
Pablo Muralles <pablomuralles28@gmail.com>
2020-09-08 11:48:00 -0600

    se hicieron los metodos hata expresion se va a probar si funciona bien

--- MiniCompilador/Laboratorio/Parseo.cs
+++ MiniCompilador/Laboratorio/Parseo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace MiniCompilador.Laboratorio
         private int contador = 0;
         private List<string> errores = new List<string>();
 
-        private bool MatchToken(string expectedToken, bool epsilon)
+        private void MatchToken(string expectedToken)
         {
             try
             {
@@ -23,15 +24,12 @@ namespace MiniCompilador.Laboratorio
                 {
                     contador++;
                     lookahead = tokens[contador].Item1;
-                    return true;
+                     
                 }
                 else
-                {
-                    if (epsilon == false)
-                    {
-                        errores.Add($"Error sintactico: se esperaba {expectedToken} y se tenia {lookahead}. {ObtenerUbicacion(tokens[contador == 0 ? contador : contador - 1].Item2)}");
-                    }
-                    return false;
+                { 
+                       errores.Add($"Error sintactico: se esperaba {expectedToken} y se tenia {lookahead}. {ObtenerUbicacion(tokens[contador == 0 ? contador : contador - 1].Item2)}");
+                    
                 }
             }
             catch (Exception)
@@ -41,64 +39,199 @@ namespace MiniCompilador.Laboratorio
             }
         }
 
-        public void EscribirErrores()
+        private void Program_()
         {
+            Decl();
+            Program_P();
             
+             
         }
 
-        private void E()
+        private void Program_P()
         {
-            T();
-            EP();
-        }
-
-        private void EP()
-        {
-            if (MatchToken("+",true))
+            if (lookahead != "$")
             {
-                T();
-                EP();
-            }
+                Program_();
+
+            } 
         }
 
-        private void T()
+        private void Decl()
         {
-            F();
-            TP();
-        }
-
-        private void TP()
-        {
-            if (MatchToken("*",true)) 
-            {
-                F();
-                TP();
-            }
-        }
-
-        private void F()
-        {
-            if (MatchToken("5", false))
-            {
-
-            }
-        }
-
-        private void FunctionProgram()
-        {
+            VariableDecl();
             FunctionDecl();
-            FunctionProgramP();
         }
 
-        private void FunctionProgramP()
+        private void VariableDecl()
         {
-            FunctionProgram();
+            Variable();
         }
+         
+        private void Variable()
+        {
+            Type();
+            // ver como vamos a manejar las tuplas
+            MatchToken("identificador");
+        }
+        private void Type()
+        {
+            if (lookahead == "int") 
+            {
+                MatchToken("int");
+                TypeP();
 
+            }
+            else if (lookahead == "double")
+            {
+                MatchToken("int");
+                TypeP();
+            }
+            else if (lookahead == "string")
+            {
+                MatchToken("string");
+                TypeP();
+            }
+            else if (lookahead == "identificador")
+            {
+                MatchToken("identificador");
+                TypeP();
+            }
+        }
+        private void TypeP()
+        {
+            if (lookahead == "[]")
+            {
+                MatchToken("[]");
+                TypeP();
+            }
+             
+        }
         private void FunctionDecl()
         {
+            if (lookahead == "void")
+            {
+                MatchToken("void");
+                MatchToken("identificiador");
+                MatchToken("(");
+                if (lookahead != ")")
+                {
+                    Formals();
+                }
+                MatchToken(")");
+
+
+            }
+            else
+            {
+                Type();
+                MatchToken("identificiador");
+                MatchToken("(");
+                if (lookahead != ")")
+                {
+                    Formals();
+                }
+                MatchToken(")");
+                
+
+
+            }
 
         }
+
+        private void Formals()
+        {
+            VariableP();
+            MatchToken(",");
+        }
+
+        private void VariableP()
+        {
+            Variable();
+            VariableP();
+        }
+
+        private void Stmt()
+        {
+            StmtP();
+            Stmt();
+        }
+        private void StmtP()
+        {
+            if (lookahead == "for")
+            {
+                ForStmt();
+            }
+            else if (lookahead == "return")
+            {
+                ReturnStmt();
+            }
+            else
+            {
+                // falta expr
+                MatchToken(";");
+            }
+        }
+
+        private void ForStmt()
+        {
+            MatchToken("for");
+            MatchToken("(");
+            if (lookahead != ")")
+            {
+                ExprP();
+            }
+            MatchToken(")");
+            MatchToken(";");
+            Expr();
+            MatchToken(";");
+            MatchToken("(");
+            if (lookahead != ")")
+            {
+                ExprP();
+            }
+            Stmt();
+
+
+        }
+
+        private void ReturnStmt()
+        {
+            MatchToken("return");
+            MatchToken("(");
+            if (lookahead != ")")
+            {
+                ExprP();
+            }
+            MatchToken(")");
+            MatchToken(";");
+        }
+        private void ExprP()
+        {
+            ExprP();
+        }
+
+        private void Expr()
+        {
+            // Lvalue = P | P 
+        }
+
+        private void Lvalue()
+        {
+            if (lookahead == "identificador")
+            {
+                MatchToken("identificador");
+            }
+            else
+            {
+                Expr();
+                if (lookahead == ".")
+                {
+                    MatchToken(".");
+                    MatchToken("identificador");
+                }
+            }
+        }
+
 
         /// <summary>
         /// Metodo para poder mostrar la linea y la columna donde se encuentra el error
@@ -125,10 +258,10 @@ namespace MiniCompilador.Laboratorio
         /// <param name="tokens_">Lista de tuplas generada en el analisis lexico</param>
         public Parseo(List<Tuple<string, string>> tokens_)
         {
-            //tokens_.Add(new Tuple<string, string>("$", " "));
+            tokens_.Add(new Tuple<string, string>("$", " "));
             tokens = tokens_;
             lookahead = tokens[contador].Item1;
-            E();
+            Program_();
         }
 
     }
