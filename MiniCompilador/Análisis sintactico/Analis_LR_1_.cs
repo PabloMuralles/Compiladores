f2ddf5634ea973408307031c2c656fca03a81af0
Santiago Bocel <santiagobocel10@gmail.com>
2020-10-19 11:32:07 -0600

    cracion de tabla analisis

--- MiniCompilador/Análisis sintactico/Analis_LR_1_.cs
+++ MiniCompilador/Análisis sintactico/Analis_LR_1_.cs
@@ -9,97 +9,13 @@ namespace Minic.Análisis_sintactico
 {
     class Analis_LR_1_
     {
-        Dictionary<int, string> tables_dictionary = new Dictionary<int, string>();
+        Dictionary<int, Dictionary<string, string>> tables_dictionary = new Dictionary<int, Dictionary<string, string>>();
         private void Import_table()
         {
-            tables_dictionary = new Dictionary<int, string>()
-            {
-               {0,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺Program☺Decl☺Reserved☺Type"},
-              {1,"$"},
-{2,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$☺Program☺Decl☺Reserved☺Type"},
-{3,"ident☺[]"},
-{4,"ident"},
-{5,"ident"},
-{6,"ident"},
-{7,"int☺double☺bool☺string☺ConstType"},
-{8,"ident☺[]"},
-{9,"ident☺[]"},
-{10,"ident☺[]"},
-{11,"ident☺[]"},
-{12,"ident☺[]"},
-{13,"ident"},
-{14,"$"},
-{15,";"},
-{16,"ident☺[]"},
-{17,"("},
-{18,"ident☺class☺{☺interface☺const☺void☺int☺double☺bool☺string☺,☺:☺$☺Id"},
-{19,"{"},
-{20,"ident"},
-{21,"ident"},
-{22,"ident"},
-{23,"ident"},
-{24,"ident"},
-{25,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$"},
-{26,"ident☺int☺double☺bool☺string☺Type☺Formals"},
-{27,"{☺,☺Id’"},
-{28,"ident"},
-{29,"ident☺}☺void☺int☺double☺bool☺string☺Type☺Prototype’☺Prototype"},
-{30,";"},
-{31,")"},
-{32,"ident☺[]"},
-{33,"{"},
-{34,"ident"},
-{35,"ident☺class☺{☺interface☺const☺void☺int☺double☺bool☺string☺,☺$"},
-{36,"}"},
-{37,"ident☺}☺void☺int☺double☺bool☺string☺Type☺Prototype’☺Prototype"},
-{38,"ident☺[]"},
-{39,"ident"},
-{40,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$"},
-{41,"{☺StmtBlock"},
-{42,")☺,☺"},
-{43,"ident☺}☺const☺void☺int☺double☺bool☺string☺Reserved☺Type☺Field’☺Field"},
-{44,"{☺,☺Id’"},
-{45,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$"},
-{46,"}"},
-{47,"("},
-{48,"("},
-{49,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$"},
-{50,"ident☺;☺(☺class☺{☺}☺interface☺const☺void☺int☺double☺bool☺string☺if☺while☺for☺break☺return☺Console☺-☺!☺this☺New☺intConstant☺doubleConstant☺boolConstant☺stringConstant☺null☺$☺Type☺VariableDecl’"},
-{51,"ident☺int☺double☺bool☺string☺Type☺Formals"},
-{52,"}"},
-{53,"ident☺}☺const☺void☺int☺double☺bool☺string☺Reserved☺Type☺Field’☺Field"},
-{54,"ident☺(☺[]"},
-{55,"("},
-{56,"int☺double☺bool☺string☺ConstType"},
-{57,"ident☺(☺[]"},
-{58,"ident☺(☺[]"},
-{59,"ident☺(☺[]"},
-{60,"ident☺(☺[]"},
-{61,"ident☺(☺[]"},
-{62,"("},
-{63,"{"},
-{64,"ident☺int☺double☺bool☺string☺Type☺Formals"},
-{65,"ident☺int☺double☺bool,string☺Type☺Formals"},
-{66,"ident☺;☺(☺class☺{☺}☺interface☺const☺void☺int☺double☺bool☺string☺if☺while☺for☺break☺return☺Console☺-☺!☺this☺New☺intConstant☺doubleConstant☺boolConstant☺stringConstant☺null☺$☺ConstDecl’"},
-{67,"ident☺[]"},
-{68,")"},
-{69,"ident☺class☺interface☺const☺void☺int☺double☺bool☺string☺$"},
-{70,"}"},
-{71,";"},
-{72,"ident☺(☺[]"},
-{73,"ident☺int☺double☺bool☺string☺Type☺Formals☺"},
-{74,"ident"},
-{75,")"},
-{76,")"},
-{77,"ident☺;☺(☺{☺}☺if☺while☺for☺break☺return☺Console☺-☺!☺this☺New☺intConstant☺doubleConstant☺boolConstant☺stringConstant☺null☺StmtBlock☺Stmt’☺Stmt☺Expr☺ConditionAnd☺Equality☺Relational☺Additive☺Multiplicative☺Unary☺Primary☺Terminal"},
-{78,"int☺double☺bool☺string☺ConstType"},
-{79,";"},
-{80,"ident☺}☺const☺void☺int☺double☺bool☺string"},
-{81,")"},
-{82,";"},
-{83,";"},
-{84,";"},
-            };
+            tables_dictionary.Add(0, );             
+
+
+
         }
         public void table(List<Tuple<string, string>> tokens_)
         {
@@ -111,8 +27,7 @@ namespace Minic.Análisis_sintactico
 
             if (tables_dictionary.ContainsKey(pila.Peek()))
             {
-                var array_symbol = tables_dictionary[pila.Peek()].Split('☺');
-                search_symbol(array_symbol);
+                
             }
             else
             {
